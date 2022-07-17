0e83feb04eb074a5bf08c876a2cfbaeeeb04e2a3
Frotas <Guilherme.frotas19@gmail.com>
2022-07-17 01:53:04 -0300

    🐛bugfix: Feedback gráfico inoperante.

--- public/javascript/jogoDaForca.js
+++ public/javascript/jogoDaForca.js
@@ -1,95 +1,102 @@
-const dicionarieWords = ["Avião", "Dinossauro", "Carro", "Ônibus", "Sapato", "Escola", "Dicionario", "Caderno"];
+import Emitter from './emitter.js';
+
+const dicionarieWords = ["Aviao", "Dinossauro", "Carro", "Onibus", "Sapato", "Escola", "Dicionario", "Caderno"];
 const alphabet = [
   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
   "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ];
-const secret = dicionarieWords[Math.floor(Math.random() * dicionarieWords.length)];
-const errors = [];
-const correct = [];
 
 const jogoDaForca = {
-  /**
-   * @param {HTMLElement} container
-   * @param {String[]} arr
-   * @param {String} text 
-   * @param {String} secret
-   * @returns {void}
-   */
-  showLetters(container, arr, text, secret) {
-    container.innerHTML = `<h3>${text}</h3>`;
-    if (text.includes('Letras Erradas')) {
-      arr.forEach((letter) => {
-        container.innerHTML += `<span> ${letter} </span>`;
-      });
-    } else {
-      secret.split("").forEach((letter) => {
-        if (arr.includes(letter)) {
-          container.innerHTML += `<span> ${letter} </span>`;
-        } else {
-          container.innerHTML += `<span> _ </span>`;
-        }
-      })
-    }
-  },
-  /**
-   * @param {HTMLElement} container 
-   * @param {String[]} arr 
-   * @param {String?} secret 
-   * @param {String} message 
-   * @returns {String}
-   */
-  isFinish(container, arr, secret, message) {
-    if (arr.length === secret.length) {
-      return message;
-    } else if (secret === container.innerText) {
-      return message;
-    }
-  },
-  draw() {
-    const partesCorpo = document.querySelectorAll(".forca-parte");
-    for (let i = 0; i < errors.length; i++) {
-      partesCorpo[i].style.display = "block";
-    }
-  },
-  /**
-   * @param {String[]} dicionarie 
-   * @returns {String | Boolean}
-   */
-  isWord(dicionarie) {
-    document.addEventListener('keyup', (e) => {
-      dicionarie.forEach((letter) => {
-        if (e.key.toUpperCase() === letter.toUpperCase()) {
-          return true;
+  logic: {
+    missWords: [],
+    hitsWords: [],
+    /**
+     * @param {NodeListOf<HTMLElement> | NodeListOf<Element>} btnVirtualKeyboard 
+     */
+    eventKeyboard(btnVirtualKeyboard) {
+      document.addEventListener('keyup', (e) => {
+        const keyCode = e.keyCode; // Alphabet UNICODE interval is 65 to 90.
+        const key = e.key;
+        if (this.isLetter(keyCode)) {
+          // console.log(key.toLowerCase());
+          // Função contento a logica do jogo
+          this.checkSecret(key, this.switchWord, btnVirtualKeyboard);
         }
       });
-    });
-  },
-
-  loadGame(secret) {
-    document.addEventListener("keyup", (e) => {
-      const keyCode = e.key; // 65 - 90 (intervalo)
-      if (this.isWord(alphabet)) {
-        console.log("OK!")
-        if (errors.includes(keyCode)) {
-
-        } else if (secret.includes(keyCode)) {
-          correct.push(keyCode)
+    },
+    /**
+     * @param {NodeListOf<HTMLElement> | NodeListOf<Element>} btnVirtualKeyboard - Elementos/Botões do Teclado Virtual.
+     * @param {String[]} alphabet - Alfabeto...
+     */
+    eventVirtualKeyboard(btnVirtualKeyboard, alphabet) {
+      btnVirtualKeyboard.forEach((element) => {
+        element.addEventListener('click', (e) => {
+          /**
+           * @type {HTMLElement | any}
+           */
+          const btn = e.target;
+          if (alphabet.includes(btn.innerText.toLowerCase())) {
+            this.checkSecret(btn.innerText.toLowerCase(), this.switchWord, btnVirtualKeyboard);
+          }
+        });
+      });
+    },
+    /**
+     * @param {Number} cod - Código UNICODE do teclado.
+     * @returns {Boolean} - Retorna se uma das teclas pressionadas é um número.
+     */
+    isLetter(cod) {
+      return cod >= 65 && cod <= 90;
+    },
+    /**
+    * @param {String[]} dicionarie - Dicionario com as palavras a serem sorteadas.
+    * @returns {String}
+    **/
+    switchWord: dicionarieWords[Math.floor(Math.random() * dicionarieWords.length)],
+    /**
+     * @param {String} key - Letra escolhida.
+     * @param {String} secret - Palavra secreta.
+     * @param {NodeListOf<HTMLElement>} keyboard - Elementos do Teclado Virtual.
+     * @param {String[]} arr
+     */
+    checkSecret(key, secret, keyboard) {
+      if (this.missWords.includes(key)) {
+        console.warn("Letra Repetida!");
+      } else {
+        if (secret.includes(key)) {
+          console.log(secret, key);
+          this.hitsWords.push(key);
+          this.showLetterHit(keyboard);
         } else {
-          errors.push(keyCode);
+          this.missWords.push(key);
+          this.showLetterMiss(keyboard);
         }
-        this.updateGame();
       }
-    });
-  },
-  updateGame() {
-    const secretContainer = document.querySelector('.palavra-secreta-container');
-    this.showLetters(document.querySelector('.letras-erradas-container'), errors, 'Letras Erradas');
-    this.showLetters(secretContainer, correct, "", secret);
-    this.draw();
-    this.isFinish("", errors, "", 'Fim de Jogo! Você perdeu!');
-    this.isFinish(secretContainer, correct, secret, 'Parabéns! Você ganhou!');
-  },
+    },
+    /**
+     * @param {NodeListOf<HTMLElement>} container
+     */
+    showLetterMiss(container) {
+      container.forEach((element) => {
+        element.addEventListener('click', (e) => {
+          /**
+           * @param {HTMLElement} btn
+          **/
+          const btn = e.target;
+          
+        })
+      })
+    },
+    /**
+     * @param {NodeListOf<HTMLElement>} container
+    **/
+    showLetterHit(container) {
+
+    }
+  }
 }
 
-jogoDaForca.loadGame();
-jogoDaForca.updateGame();
+const keyboard = document.querySelectorAll('.btn-keyboard');
+jogoDaForca.logic.eventVirtualKeyboard(keyboard, alphabet);
+jogoDaForca.logic.eventKeyboard(keyboard);
+
